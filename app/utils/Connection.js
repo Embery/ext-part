42f6135735c416e390018efbf9ce7b651434fcfa
Svetlana Romashkina <s.romashkina@i-t-p.pro>
2022-04-26 21:46:23 +0300

    url and links

--- app/utils/Connection.js
+++ app/utils/Connection.js
@@ -2,9 +2,14 @@ Ext.define('MyApp.utils.Connection', {
     extend: 'Ext.data.Connection',
     singleton: true,
     request(options){
+        const isLocalhost = /localhost/.test(location.host);
+        const url = isLocalhost ? 
+            options.url : 
+            options.url.replace(/http:\/\/localhost:(\d+)/, 'https://ext-plus-react.herokuapp.com')
         return new Promise((resolve, reject) => {
             Ext.Ajax.request({
                 ...options, 
+                url,
                 headers: {
                     ...(options.headers || {}),
                     Authorization: `Bearer ${localStorage.getItem('bearer')}`
--- app/view/mainTabPanel/MainTabPanel.js
+++ app/view/mainTabPanel/MainTabPanel.js
@@ -17,7 +17,9 @@
                 <p style="text-align: center;">Добро пожаловать на фракенштейна, собранного из эксты и react + mobx + antd!<br>
                 Здесь написанные с любовью компоненты экспортируются в виде глобальной переменной ReactLibrary<br>
                 Можно прокидывать пропсы, можно прокидывать сторы, можно развлекаться как угодно, взяв это за основу<br>
-                Ссылки: <a href="#" target="_blank">экстовая часть</a> и <a href="#" target="_blank">реактовая часть<a></p>
+                Ссылки: 
+                <a href="https://github.com/Embery/ext-part.git" target="_blank">экстовая часть</a> и 
+                <a href="https://github.com/Embery/react-part" target="_blank">реактовая часть<a></p>
             `,
         },
         {
